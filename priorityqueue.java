30c572c3e334300c6d93c36fdffb28f6799e58ef
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-03-27 18:57:11 +0530

    Add files via upload
    Priority Queue implementation in java

---
+++ priorityqueue.java
@@ -0,0 +1,16 @@
+package Heap_Priority_Queues;
+import java.util.*;
+public class priorityqueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());
+            pq.remove();
+
+        }
+    }
+}
